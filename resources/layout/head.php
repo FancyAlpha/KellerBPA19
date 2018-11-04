72dd4108bd3dc087518311241889d5816fe3d818
tarakapranav <tarakapranav@gmail.com>
2018-11-04 00:50:32 -0500

    preliminary navigation bar added

--- resources/layout/head.php
+++ resources/layout/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Testing</title>
+    <title>Hello World</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="resources/styles/base.css"">
--- resources/layout/nav.php
+++ resources/layout/nav.php
@@ -1,3 +1,25 @@
-<nav>
+<nav class="navbar justify-content-between navbar-expand-sm navbar-dark bg-dark"">
 
+    <a class="navbar-brand" href="#">NLC Washington</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="navbar-nav ml-auto">
+
+            <a class="nav-item nav-link" href="#">Locations</a></li>
+
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Schedule
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Day One</a>
+                    <a class="dropdown-item" href="#">Day Two</a>
+                    <a class="dropdown-item" href="#">Day Three</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </nav>
