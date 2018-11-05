798ac1e8803c65b81808cae2748b4e41bee45757
tarakapranav <tarakapranav@gmail.com>
2018-11-04 19:37:46 -0600

    navbar updated to work perfectly

--- resources/layout/nav.php
+++ resources/layout/nav.php
@@ -1,37 +1,47 @@
-<nav class="navbar navbar-expand-sm navbar-light bg-light pt-4">
-    <div class="container flex-column">
-        <div class="justify-content-center">
-            <a href="index.php" class="navbar-brand p-3 bg-dark text-white">
-                BPA's NLC 2020 in Washington D.C
-            </a>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container d-flex flex-column">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse pt-3" id="navbarNavDropdown">
-            <div class="navbar-nav ml-2">
+        <div class="">
+            <a href="index.php" class="navbar-brand p-3 bg-dark text-white">
+                BPA NLC 2020
+            </a> <span class="d-none d-md-inline">in Washington D.C</span>
+        </div>
 
-                <a class="nav-item nav-link" href="planner.php">Locations</a><span class="navbar-text">&nbsp;|&nbsp;</span>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="navbar-nav">
+
+                <a class="nav-item nav-link" href="planner.php">Locations</a><span
+                        class="navbar-text d-none d-lg-block">&nbsp;|&nbsp;</span>
 
                 <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="schedule.php" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="schedule.php" data-toggle="dropdown"
+                       aria-haspopup="true" aria-expanded="false">
                         Schedule
                     </a>
+
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                         <a class="dropdown-item" href="schedule.php#dy1">Day One</a>
                         <a class="dropdown-item" href="schedule.php#dy2">Day Two</a>
                         <a class="dropdown-item" href="schedule.php#dy3">Day Three</a>
                     </div>
-                </div><span class="navbar-text">&nbsp;|&nbsp;</span>
+                </div>
+                <span class="navbar-text d-none d-lg-block">&nbsp;|&nbsp;</span>
 
 
-                <a class="nav-item nav-link" href="#">Events/Competitions</a><span class="navbar-text">&nbsp;|&nbsp;</span>
-                <a class="nav-item nav-link" href="#">Competition Info</a><span class="navbar-text">&nbsp;|&nbsp;</span>
-                <a class="nav-item nav-link" href="#">About BPA</a><span class="navbar-text">&nbsp;|&nbsp;</span>
+                <a class="nav-item nav-link" href="#">Events/Competitions</a><span
+                        class="navbar-text d-none d-lg-block">&nbsp;|&nbsp;</span>
+                <a class="nav-item nav-link" href="#">Competition Info</a><span class="navbar-text d-none d-lg-block">&nbsp;|&nbsp;</span>
+                <a class="nav-item nav-link" href="#">About BPA</a><span class="navbar-text d-none d-lg-block">&nbsp;|&nbsp;</span>
                 <a class="nav-item nav-link" href="#">Contact Us</a>
             </div>
         </div>
+
     </div>
 </nav>
