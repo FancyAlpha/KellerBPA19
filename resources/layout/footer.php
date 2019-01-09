bfa1824d6873215420c6e32ea5ea4d58aa8994db
tarakapranav <tarakapranav@gmail.com>
2019-01-09 22:32:01 +0530

    gave footer some more love (needs fixing for small screens

--- resources/layout/footer.php
+++ resources/layout/footer.php
@@ -1,41 +1,57 @@
 <footer class="p-5 shadow-lg">
-    <div class="row container justify-content-center m-auto pt-5">
+    <div class="container">
 
-        <div class="col-auto px-5">
-            <div class="col justify-content-center">
-                <a href="http://www.bpa.org/"><img class="logo" src="../../resources/assets/logo-white.gif"></a>
+        <div class="row justify-content-center">
+
+            <div class="row col-auto pt-5">
+                <div class="col justify-content-center pb-2">
+                    <a href="http://www.bpa.org/"><img class="logo" src="../../resources/assets/logo-white.gif"></a>
+                </div>
+
+                <div class="col text-light">
+                    <p class="mb-2">
+                    <i class="far fa-clock"></i> <b>Timings</b><br>
+                    Monday&nbsp;-&nbsp;Friday 8am&nbsp;-&nbsp;5pm&nbsp;EST
+                    </p>
+                    <p class="mb-2"><i class="fas fa-phone"></i> 800-334-2007</p>
+                    <p class="mb-2"><i class="fas fa-map-marker-alt"></i> 5454&nbsp;Cleveland&nbsp;Avenue
+                    Columbus,&nbsp;OH</p>
+                </div>
             </div>
 
-            <div class="row fa-2x text-center social-strip">
-                <a class="twitter" href="https://twitter.com/National_BPA">
-                    <i class="fab fa-twitter"></i></a>
 
-                <a class="facebook" href="https://www.facebook.com/businessprofessionalsofamerica/">
-                    <i class="fab fa-facebook-f"></i></a>
+            <div class="row col-auto col-lg-4 pt-5 footer-links">
+                <div class="col">
+                    <a href="planner.php">Locations</a><br>
+                    <a href="locations.php">Schedule</a><br>
+                    <a href="#">Event List</a><br>
+                    <a href="about.php">About Us</a><br>
+                </div>
 
-                <a class="linkedin" href="https://www.linkedin.com/company/business-professionals-of-america">
-                    <i class="fab fa-linkedin-in"></i></a>
-
-                <a class="instagram" href="https://www.instagram.com/bpanational/?hl=en">
-                    <i class="fab fa-instagram"></i></a>
-            </div>
-        </div>
-
-        <div class="col-auto row footer-links px-5">
-            <div class="col">
-                <a href="planner.php">Locations</a><br>
-                <a href="locations.php">Schedule</a><br>
-                <a href="#">Event List</a><br>
-                <a href="about.php">About Us</a><br>
+                <div class="col">
+                    <a href="http://www.bpa.org/">BPA.org</a><br>
+                    <a href="http://www.bpa.org/compete/downloadcenter">WSAP Download Center</a><br>
+                    <a href="http://www.bpa.org/about">About BPA</a><br>
+                </div>
             </div>
 
-            <div class="col">
-                <a href="http://www.bpa.org/">BPA.org</a><br>
-                <a href="http://www.bpa.org/compete/downloadcenter">WSAP Download Center</a><br>
-                <a href="http://www.bpa.org/about">About BPA</a><br>
-            </div>
         </div>
     </div>
+
+    <div class="fa-2x text-center social-strip mt-5">
+        <a class="twitter" href="https://twitter.com/National_BPA">
+            <i class="fab fa-twitter"></i></a>
+
+        <a class="facebook" href="https://www.facebook.com/businessprofessionalsofamerica/">
+            <i class="fab fa-facebook-f"></i></a>
+
+        <a class="linkedin" href="https://www.linkedin.com/company/business-professionals-of-america">
+            <i class="fab fa-linkedin-in"></i></a>
+
+        <a class="instagram" href="https://www.instagram.com/bpanational/?hl=en">
+            <i class="fab fa-instagram"></i></a>
+    </div>
+
     <div class="row mt-5">
         <div class="col text-center text-muted">COPYRIGHT &copy; 2019 BUSINESS PROFESSIONALS OF AMERICA. ALL RIGHTS RESERVED.</div>
     </div>
